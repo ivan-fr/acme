b7612440556a21b1119ca86ea6aae56e37a5076c
Besevic <ivan.besevic_fr@yahoo.com>
2018-08-27 16:46:45 +0200

    fix

--- publication/views.py
+++ publication/views.py
@@ -1,9 +1,8 @@
-from django.contrib.sites.shortcuts import get_current_site
 from django.views.generic.dates import ArchiveIndexView, MonthArchiveView, YearArchiveView, WeekArchiveView
-from django.views.generic.detail import DetailView
-from django.http import Http404
-from django.utils.translation import ugettext_lazy as _
+from django.views.generic.list import ListView
 from django.contrib.auth.views import redirect_to_login
+from django.http import Http404
+
 from publication.models import Entry, FlatPage
 from page.views import repertory_kwarg, MenuDetailView
 
@@ -16,8 +15,8 @@ class EntryMixinView(object):
     allow_empty = False
 
     def get(self, request, *args, **kwargs):
-        directory = kwargs.get(repertory_kwarg)
-        _kwargs = {repertory_kwarg: directory}
+        menu_repertory = kwargs.get(repertory_kwarg)
+        _kwargs = {repertory_kwarg: menu_repertory}
         self.menu_context = MenuDetailView.as_view()(request, **_kwargs)
 
         return super(EntryMixinView, self).get(request, *args, **kwargs)
@@ -49,29 +48,38 @@ class EntryWeekArchiveView(EntryMixinView, WeekArchiveView):
     week_format = "%W"
 
 
-class FlatPageView(DetailView):
+class FlatPageView(ListView):
     model = FlatPage
+    paginate_by = 1
+    allow_empty = False
 
     def get(self, request, *args, **kwargs):
+        menu_repertory = kwargs.get(repertory_kwarg)
+        _kwargs = {repertory_kwarg: menu_repertory}
+        self.menu_context = MenuDetailView.as_view()(request, **_kwargs)
+
         super(FlatPageView, self).get(request, *args, **kwargs)
 
-        self.object = self.get_object(self.get_queryset())
+        self.object_list = self.get_queryset().filter(menu=self.menu_context['page_menu_object']).select_related('user')
+        allow_empty = self.get_allow_empty()
 
-        if self.object.registration_required and not request.user.is_authenticated:
+        if not allow_empty:
+            # When pagination is enabled and object_list is a queryset,
+            # it's better to do a cheap query than to load the unpaginated
+            # queryset in memory.
+            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
+                is_empty = not self.object_list.exists()
+            else:
+                is_empty = not self.object_list
+            if is_empty:
+                raise Http404(_("Empty list and '%(class_name)s.allow_empty' is False.") % {
+                    'class_name': self.__class__.__name__,
+                })
+        context = self.get_context_data()
+
+        paginated_object = context['object_list']
+
+        if all(item.registration_required for item in paginated_object) and not request.user.is_authenticated:
             return redirect_to_login(request.path)
 
-        context = self.get_context_data(object=self.object)
         return self.render_to_response(context)
-
-    def get_object(self, queryset=None):
-        try:
-            obj = queryset.get()
-        except queryset.model.DoesNotExist:
-            raise Http404(_("No %(verbose_name)s found matching the query") %
-                          {'verbose_name': queryset.model._meta.verbose_name})
-        return obj
-
-    def get_queryset(self):
-        queryset = super(FlatPageView, self).get_queryset()
-        site_id = get_current_site(self.request).id
-        return queryset.filter(sites=site_id)
